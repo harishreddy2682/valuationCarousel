f8c789f82a6de3b20e474788e50de8f4b8d95041
holydefish <harishreddy.gamer@gmail.com>
2022-04-30 01:11:16 +0530

    Finished leaderboard.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ValuationCarousel from './valuation_carousel/ValuationCarousel'
-import Leaderboard from './leaderboard/Leaderboard';
+import Leaderboard from './leaderboard/Leaderboard'
 
 function App() {
   return (
--- src/leaderboard/Leaderboard.js
+++ src/leaderboard/Leaderboard.js
@@ -3,142 +3,187 @@ import styles from './Leaderboard.module.scss'
 
 const Leaderboard = () => {
 
-    const countries = [
-        'India',
-        'Australia',
-        'Russia',
-        'UAE',
-        'USA'
-    ]
-
     const usersData = [
         {
             username: 'kalyan_varma',
             name: 'Kalyan Verma',
             image: '/images/leaderboard/profile.png',
-            coinsInWallet: '12,09,00,000',
-            growthStatus: 'positive'
+            country: '/images/leaderboard/country.svg',
+            coinsInWallet: '52,65,000',
+            indicator: 'replay'
         },
         {
             username: 'kalyan_varma',
             name: 'Kalyan Verma',
             image: '/images/leaderboard/profile.png',
-            coinsInWallet: '12,09,00,000',
-            growthStatus: 'neutral'
+            country: '/images/leaderboard/country.svg',
+            coinsInWallet: '52,65,000',
+            indicator: 'negative'
         },
         {
             username: 'kalyan_varma',
             name: 'Kalyan Verma',
             image: '/images/leaderboard/profile.png',
-            coinsInWallet: '12,09,00,000',
-            growthStatus: 'negative'
+            country: '/images/leaderboard/country.svg',
+            coinsInWallet: '52,65,000',
+            indicator: 'positive'
         },
         {
             username: 'kalyan_varma',
             name: 'Kalyan Verma',
             image: '/images/leaderboard/profile.png',
-            coinsInWallet: '12,09,00,000',
-            growthStatus: 'positive'
+            country: '/images/leaderboard/country.svg',
+            coinsInWallet: '52,65,000',
+            indicator: 'positive'
         },
     ]
-
-    const [users, setUsers] = useState(usersData)
-
-    const [selectedCountry, setSelectedCountry] = useState(countries[0])
-    const [search, setSearch] = useState('')
-
+    // eslint-disable-next-line
+    const [users, setUser] = useState(usersData)
 
     return (
-        <div className={styles.container}>
-            <span className={styles.mobileHeader}>
-                <img src="/images/leaderboard/arrowLeft.svg" alt="" />
-                <h1>Leaderboard</h1>
-                <div className={styles.searchMobile}>
-                    <img src="/images/leaderboard/search.svg" alt="" />
-                    <input type="text" id='search' name='search' placeholder='Search...' value={search} onChange={e => setSearch(e.target.value)} />
+        <div className={styles.leaderboardRoot}>
+            <div className={styles.container}>
+                <div className={styles.mobileHeader}>
+                    <img src="images/leaderboard/arrowLeft.svg" alt="back" />
+                    <p>Leaderboard</p>
+                    <img src="images/leaderboard/search.svg" alt="search" />
                 </div>
-            </span>
+                <div className={styles.filterDiv}>
+                    <p>Filter Rank By: </p>
+                    <button id={styles.globalBtn}>Global</button>
+                    <button id={styles.countryBtn}>Country</button>
+                    <button id={styles.friendsBtn}>Friends</button>
 
-            <div className={styles.filter}>
-                <p>Filter Rank By: </p>
-                <button id={styles.globalBtn}>Global</button>
-                <button id={styles.countryBtn}>Country</button>
-                <button id={styles.friendsBtn}>Friends</button>
-                <select name="country" id="country" onChange={e => setSelectedCountry(e.target.value)}>
-                    {countries.map((i) => <option key={i} value={i}>{i}</option>)}
-                </select>
-                <div className={styles.search}>
-                    <img src="/images/leaderboard/search.svg" alt="" />
-                    <input type="text" id='search' name='search' placeholder='Search...' value={search} onChange={e => setSearch(e.target.value)} />
-                </div>
-            </div>
-
-            <div className={styles.top3_container}>
-                <div id={styles.top_2} className={styles.top3_wrapper}>
-                    <img src='/images/leaderboard/crown_silver.svg' className={styles.crown} alt="" />
-                    <div className={styles.avatarWrapper}>
-                        <img className={styles.userImg} src={usersData[1].image} alt="avatar" />
-                        <img className={styles.userCountry} src="/images/leaderboard/country.svg" alt="" />
+                    <div className={styles.search}>
+                        <img src="/images/leaderboard/search.svg" alt="" />
+                        <input type="text" id='search' name='search' placeholder='Search...' />
                     </div>
-                    <h1 >{usersData[1].username}</h1>
-                    <span>
-                        <img src="/images/leaderboard/coins.svg" alt="" />
-                        <p>{usersData[1].coinsInWallet}</p>
+                </div>
+
+                <div className={styles.listTitleDiv}>
+                    <p className={styles.rank}>Rank</p>
+                    <p className={styles.country}>Country</p>
+                    <p className={styles.profile}>Profile</p>
+                    <p className={styles.ucoins}>U-Coins</p>
+                    <p className={styles.indicator}>Indicator</p>
+                </div>
+
+                <div className={styles.top1}>
+                    <img className={styles.crown} src="images/leaderboard/crown_gold.svg" alt="1" />
+                    <span className={`${styles.mobileAvatarBox} ${styles.mobile}`}>
+                        <img className={styles.mobileAvatar} src={usersData[0].image} alt="" />
+                        <img className={styles.mobileCountry} src="images/leaderboard/countryRound.svg" alt="" />
                     </span>
-                </div>
-                <div id={styles.top_1} className={styles.top3_wrapper}>
-                    <img src='/images/leaderboard/crown_gold.svg' className={styles.crown} alt="" />
-                    <div className={styles.avatarWrapper}>
-                        <img className={styles.userImg} src={usersData[0].image} alt="avatar" />
-                        <img className={styles.userCountry} src="/images/leaderboard/country.svg" alt="" />
+                    <div className={`${styles.mobileUsernameBox} ${styles.mobile}`}>
+                        <p>{usersData[0].username}</p>
+                        <span>
+                            <img src="/images/leaderboard/coins.svg" alt="" />
+                            <p>{usersData[0].coinsInWallet}</p>
+                        </span>
                     </div>
-                    <h1 >{usersData[0].username}</h1>
-                    <span>
-                        <img src="/images/leaderboard/coins.svg" alt="" />
+                    <img className={styles.country} src={usersData[0].country} alt="" />
+                    <img className={styles.avatar} src={usersData[0].image} alt="" />
+                    <span className={styles.usernameSpan}>
+                        <p className={styles.username}>{usersData[0].username}</p>
+                        <p>{usersData[0].name}</p>
+                    </span>
+                    <span className={styles.coinsSpan}>
+                        <img className={styles.top1Coin} src="/images/leaderboard/coins.svg" alt="" />
                         <p>{usersData[0].coinsInWallet}</p>
                     </span>
+                    {usersData[0].indicator === "positive" ? <img className={styles.indicator} src="/images/leaderboard/triangleUp.svg" alt="" /> : usersData[0].indicator === "negative" ? <img className={styles.indicator} src="/images/leaderboard/triangleDown.svg" alt="" /> : usersData[0].indicator === "plus" ? <p className={styles.indicator}>+</p> : usersData[0].indicator === "replay" ? <img className={styles.indicator} src="/images/leaderboard/replay.svg" alt="" /> : <span className={styles.indicator}>
+                        <img src="/images/leaderboard/triangleLeft.svg" alt="" />
+                        <img src="/images/leaderboard/triangleRight.svg" alt="" />
+                    </span>}
                 </div>
-                <div id={styles.top_3} className={styles.top3_wrapper}>
-                    <img src='/images/leaderboard/crown_bronze.svg' className={styles.crown} alt="" />
-                    <div className={styles.avatarWrapper}>
-                        <img className={styles.userImg} src={usersData[2].image} alt="avatar" />
-                        <img className={styles.userCountry} src="/images/leaderboard/country.svg" alt="" />
+                <div className={styles.top2}>
+                    <img className={styles.crown} src="images/leaderboard/crown_silver.svg" alt="2" />
+                    <span className={`${styles.mobileAvatarBox} ${styles.mobile}`}>
+                        <img className={styles.mobileAvatar} src={usersData[1].image} alt="" />
+                        <img className={styles.mobileCountry} src="images/leaderboard/countryRound.svg" alt="" />
+                    </span>
+                    <div className={`${styles.mobileUsernameBox} ${styles.mobile}`}>
+                        <p>{usersData[1].username}</p>
+                        <span>
+                            <img src="/images/leaderboard/coins.svg" alt="" />
+                            <p>{usersData[1].coinsInWallet}</p>
+                        </span>
                     </div>
-                    <h1 >{usersData[2].username}</h1>
-                    <span>
-                        <img src="/images/leaderboard/coins.svg" alt="" />
+                    <img className={styles.country} src={usersData[1].country} alt="" />
+                    <img className={styles.avatar} src={usersData[1].image} alt="" />
+                    <span className={styles.usernameSpan}>
+                        <p className={styles.username}>{usersData[1].username}</p>
+                        <p>{usersData[1].name}</p>
+                    </span>
+                    <span className={styles.coinsSpan}>
+                        <img className={styles.top1Coin} src="/images/leaderboard/coins.svg" alt="" />
+                        <p>{usersData[1].coinsInWallet}</p>
+                    </span>
+                    {usersData[1].indicator === "positive" ? <img className={styles.indicator} src="/images/leaderboard/triangleUp.svg" alt="" /> : usersData[1].indicator === "negative" ? <img className={styles.indicator} src="/images/leaderboard/triangleDown.svg" alt="" /> : usersData[1].indicator === "plus" ? <p className={styles.indicator}>+</p> : usersData[1].indicator === "replay" ? <img className={styles.indicator} src="/images/leaderboard/replay.svg" alt="" /> : <span className={styles.indicator}>
+                        <img src="/images/leaderboard/triangleLeft.svg" alt="" />
+                        <img src="/images/leaderboard/triangleRight.svg" alt="" />
+                    </span>}
+                </div>
+                <div className={styles.top3}>
+                    <img className={styles.crown} src="images/leaderboard/crown_bronze.svg" alt="3" />
+                    <span className={`${styles.mobileAvatarBox} ${styles.mobile}`}>
+                        <img className={styles.mobileAvatar} src={usersData[2].image} alt="" />
+                        <img className={styles.mobileCountry} src="images/leaderboard/countryRound.svg" alt="" />
+                    </span>
+                    <div className={`${styles.mobileUsernameBox} ${styles.mobile}`}>
+                        <p>{usersData[2].username}</p>
+                        <span>
+                            <img src="/images/leaderboard/coins.svg" alt="" />
+                            <p>{usersData[2].coinsInWallet}</p>
+                        </span>
+                    </div>
+                    <img className={styles.country} src={usersData[2].country} alt="" />
+                    <img className={styles.avatar} src={usersData[2].image} alt="" />
+                    <span className={styles.usernameSpan}>
+                        <p className={styles.username}>{usersData[2].username}</p>
+                        <p>{usersData[2].name}</p>
+                    </span>
+                    <span className={styles.coinsSpan}>
+                        <img className={styles.top1Coin} src="/images/leaderboard/coins.svg" alt="" />
                         <p>{usersData[2].coinsInWallet}</p>
                     </span>
+                    {usersData[2].indicator === "positive" ? <img className={styles.indicator} src="/images/leaderboard/triangleUp.svg" alt="" /> : usersData[2].indicator === "negative" ? <img className={styles.indicator} src="/images/leaderboard/triangleDown.svg" alt="" /> : usersData[2].indicator === "plus" ? <p className={styles.indicator}>+</p> : usersData[2].indicator === "replay" ? <img className={styles.indicator} src="/images/leaderboard/replay.svg" alt="" /> : <span className={styles.indicator}>
+                        <img src="/images/leaderboard/triangleLeft.svg" alt="" />
+                        <img src="/images/leaderboard/triangleRight.svg" alt="" />
+                    </span>}
                 </div>
-            </div>
-
-            <div className={styles.topUsersContainer}>
-                {users.map((user, index) => (
-                    <div key={index} className={styles.userWrapper}>
-                        <p className={styles.userIndex}>{index + 4}</p>
-                        <img className={styles.userCountry} src="/images/leaderboard/countryRectangle.svg" alt="" />
-                        <div style={{ "height": "fit-content", "width": "fit-content", "position": "relative" }}>
-                            <img className={styles.userAvatar} src={user.image} alt="" />
-                            <img className={styles.userCountryPhone} src="/images/leaderboard/country.svg" alt="" />
-                        </div>
-                        <span className={styles.nameWrapper}>
-                            <p className={styles.userUsername}>{user.username}</p>
-                            <p className={styles.userName}>{user.name}</p>
-                            <span>
-                                <img src="/images/leaderboard/coins.svg" alt="" />
+                {
+                    users.map((user, index) => (
+                        <div className={styles.singleList} key={index}>
+                            <p className={styles.index}>{index + 4}</p>
+                            <span className={`${styles.mobileAvatarBox} ${styles.mobile}`}>
+                                <img className={styles.mobileAvatar} src={usersData[2].image} alt="" />
+                                <img className={styles.mobileCountry} src="images/leaderboard/countryRound.svg" alt="" />
+                            </span>
+                            <div className={`${styles.mobileUsernameBox} ${styles.mobile}`}>
+                                <p>{usersData[2].username}</p>
+                                <span>
+                                    <img src="/images/leaderboard/coins.svg" alt="" />
+                                    <p>{usersData[2].coinsInWallet}</p>
+                                </span>
+                            </div>
+                            <img className={styles.country} src={user.country} alt="" />
+                            <img className={styles.avatar} src={user.image} alt="" />
+                            <span className={styles.usernameSpan}>
+                                <p className={styles.username}>{user.username}</p>
+                                <p>{user.name}</p>
+                            </span>
+                            <span className={styles.coinsSpan}>
+                                <img className={styles.top1Coin} src="/images/leaderboard/coins.svg" alt="" />
                                 <p>{user.coinsInWallet}</p>
                             </span>
-                        </span>
-                        <img className={styles.coin} src="/images/leaderboard/coins.svg" alt="" />
-                        <p className={styles.userWalletCoins}>{user.coinsInWallet}</p>
-
-                        {user.growthStatus === "positive" ? <img className={styles.growthIndicator} src="/images/leaderboard/triangleUp.svg" alt="" /> : user.growthStatus === "negative" ? <img className={styles.growthIndicator} src="/images/leaderboard/triangleDown.svg" alt="" /> : <span className={styles.growthIndicator}>
-                            <img src="/images/leaderboard/triangleLeft.svg" alt="" />
-                            <img src="/images/leaderboard/triangleRight.svg" alt="" />
-                        </span>}
-
-                    </div>
-                ))}
+                            {user.indicator === "positive" ? <img className={styles.indicator} src="/images/leaderboard/triangleUp.svg" alt="" /> : user.indicator === "negative" ? <img className={styles.indicator} src="/images/leaderboard/triangleDown.svg" alt="" /> : user.indicator === "plus" ? <p className={styles.indicator}>+</p> : user.indicator === "replay" ? <img className={styles.indicator} src="/images/leaderboard/replay.svg" alt="" /> : <span className={styles.indicator}>
+                                <img src="/images/leaderboard/triangleLeft.svg" alt="" />
+                                <img src="/images/leaderboard/triangleRight.svg" alt="" />
+                            </span>}
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
